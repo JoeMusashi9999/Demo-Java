c1b6272d5333fa0d09747aa9d809cc02b963ee43
Joe Musashi <63985175+JoeMusashi9999@users.noreply.github.com>
2022-06-18 13:35:48 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.awt.Graphics2D
+import 
+public class characterstats {
+  int lobbyspawn =
+  int health = 100 + changeinhealth;
+  int speed = 10 + schangeinspeed;
+  jump = 2 + changeinjump;
+  aimspread = gunspread;
+  }
